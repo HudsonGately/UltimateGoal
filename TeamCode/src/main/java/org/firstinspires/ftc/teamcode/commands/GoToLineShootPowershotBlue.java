bf1dbfb16cd87bce16c364d339672cbc9856115e
Thomas Chacko <tom.r.chacko@gmail.com>
2021-02-02 14:48:17 -0600

    changed to parallelcommandgroup instead of deadline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GoToLineShootPowershotBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GoToLineShootPowershotBlue.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.ParallelDeadlineGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.Trajectories;
 import org.firstinspires.ftc.teamcode.commands.drive.TrajectoryFollowerCommand;
 import org.firstinspires.ftc.teamcode.commands.drive.TurnCommand;
+import org.firstinspires.ftc.teamcode.commands.drive.TurnToCommand;
 import org.firstinspires.ftc.teamcode.commands.shooter.ShootRingsCommand;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.ShooterFeeder;
@@ -21,8 +23,8 @@ public class GoToLineShootPowershotBlue extends SequentialCommandGroup {
     public GoToLineShootPowershotBlue(Drivetrain drivetrain, ShooterWheels shooterWheels, ShooterFeeder feeder) {
         addCommands(
                 new TrajectoryFollowerCommand(drivetrain, Trajectories.driveToShoot),
-                new ParallelDeadlineGroup(new ShootRingsCommand(shooterWheels, feeder, 2620, 3, 150), new TurnCommand(drivetrain, -20)),
-                new TurnCommand(drivetrain, 20)
+                new ParallelCommandGroup(new ShootRingsCommand(shooterWheels, feeder, 2620, 3, 150), new TurnCommand(drivetrain, -20)),
+                new TurnToCommand(drivetrain, 0)
                 );
     }
 }
